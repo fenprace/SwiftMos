22b2089b5f7c4152f0e350c49c7e46ec0a5c39e3
Zhuo FENG <fenprace.i@gmail.com>
2022-06-15 01:06:43 +0800

    Initial Commit

---
+++ SwiftMos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftMos
+//
+//  Created by Zhuo FENG on 2022/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftMos/SwiftMosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMosApp.swift
+//  SwiftMos
+//
+//  Created by Zhuo FENG on 2022/6/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
